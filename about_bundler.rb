9325088c0f840da02e0617e8c73255457627b52f
Aleksey Drobnych <adrobnych@gmail.com>
2011-12-23 01:20:25 +0200

    3 new tests for 'about bundler'

--- about_bundler.rb
+++ about_bundler.rb
@@ -11,18 +11,32 @@ class AboutBundler < EdgeCase::Koan
     assert rails_app_directory.entries.include? "Gemfile"
   end
   
-  # you must have rails gem in Gemfile
-  def test_rails_gem_presence_in_gemfile
-    
-    
+  # you have to execute "bundle install"
+  # inside firstapp directory
+  def test_bundle_install_command
+    assert_equal :yes, bundle_install_was_executed?
   end
   
-  # you must have sqlite gem in Gemfile
+  def bundle_install_was_executed?
+    :yes
+  end
   
-  # execute "bundle install"
-  
-  # veryfy if bundler loaded gems successfuly
-  
-  
+  # check if you installed rails 3 gem using bundler
+  def test_rails3_gem_installation
+    koans_dir = Dir.pwd                               # rework using sandwich
+    Dir.chdir "../firstapp"
+    bundle_result = `bundle show rails`
+    Dir.chdir koans_dir
+    assert_equal "rails-3.", bundle_result[/rails-3./]
+  end
  
+  # check if you installed sqilite3 gem using bundler
+  def test_sqlite3_gem_installation
+    koans_dir = Dir.pwd                               # rework using sandwich
+    Dir.chdir "../firstapp"
+    bundle_result = `bundle show sqlite3`
+    Dir.chdir koans_dir
+    assert_equal "sqlite3-1.", bundle_result[/sqlite3-1./]
+  end
+  
 end
